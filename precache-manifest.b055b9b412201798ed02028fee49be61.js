debbd8a2f424688ad56a8811d8ec45160b63f33b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-02-08 21:49:50 +0000

    Deploy yaqwsx/jlcparts to github.com/yaqwsx/jlcparts.git:gh-pages

---
+++ precache-manifest.b055b9b412201798ed02028fee49be61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c61dea5f273354f4957ac994f4bd45c",
+    "url": "https://yaqwsx.github.io/jlcparts/index.html"
+  },
+  {
+    "revision": "10919fb28dbc93f94577",
+    "url": "https://yaqwsx.github.io/jlcparts/static/css/main.c942f41f.chunk.css"
+  },
+  {
+    "revision": "5b4def9f1fdf044f0a16",
+    "url": "https://yaqwsx.github.io/jlcparts/static/js/2.141c090a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://yaqwsx.github.io/jlcparts/static/js/2.141c090a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10919fb28dbc93f94577",
+    "url": "https://yaqwsx.github.io/jlcparts/static/js/main.9f136a7a.chunk.js"
+  },
+  {
+    "revision": "a31c8f175487c705797c",
+    "url": "https://yaqwsx.github.io/jlcparts/static/js/runtime-main.ca322365.js"
+  }
+]);
